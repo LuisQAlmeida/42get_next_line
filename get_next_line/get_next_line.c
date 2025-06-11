d38e69d4d841adb29c31b06c64f9c0831adc2eb8
LuisQAlmeida <lmanuel-@student.42porto.com>
2025-06-11 19:04:25 +0100

    Begin implementing get_next_line function

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -2,5 +2,25 @@
 
 char    *get_next_line(int fd)
 {
-        static  *stash;
+        static char     *buffer[BUFFER_SIZE + 1];
+        ssize_t         bytes_read;
+        char            *nxt_line;
+        int             full_line;
+
+        full_line = 0;
+        if (fd < 0 || BUFFER_SIZE <= 0 || BUFFER_SIZE > MAX_BUFFER_SIZE)
+                return (NULL);
+        while (!full_line)
+        {
+                bytes_read = (fd, buffer, BUFFER_SIZE);
+                if (!bytes_read)
+                        return (NULL);
+                else if (!bytes_read == -1)
+                        return ("READ() ERROR");
+                else
+                {
+                        ft_build_line(buffer, nxt_line);
+                }
+        }
+        return (nxt_line);
 }
