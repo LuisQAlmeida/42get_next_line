babe5eb5dba44673774ff1a696dfb130a46dc69b
LuisQAlmeida <lmanuel-@student.42porto.com>
2025-06-11 19:06:49 +0100

    Add ft_strlen_nl and ft_build_line functions

--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -1,1 +1,20 @@
+#include "get_next_line.h"
 
+int     ft_strlen_nl(char *str)
+{
+        int     len;
+
+        len = 0;
+        while (str[len] && str[len] != '\n')
+                len++;
+        return (len);
+}
+
+char    *ft_build_line(char *buf, char *nxt)
+{
+
+        nxt = malloc((ft_strlen_nl(buf) + ft_strlen_nl(nxt) + 1) * sizeof(char));
+        if (!nxt)
+                return (0);
+        return (nxt);
+}
