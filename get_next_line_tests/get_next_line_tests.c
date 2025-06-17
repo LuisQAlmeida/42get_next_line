8018572247b52abfd6eeacb89deb90944a783d43
Luís Quental Almeida <luis.quental.almeida@gmail.com>
2025-06-17 09:50:13 +0100

    Add final version of test (mandatory part)

--- get_next_line_tests/get_next_line_tests.c
+++ get_next_line_tests/get_next_line_tests.c
@@ -1,19 +1,29 @@
 #include <fcntl.h>
 #include <stdio.h>
+#include "get_next_line.h"
 
-int     main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
-        int             fd;
-        char    *nxt_line;
+	int		fd;
+	char	*nxt_line;
 
-        if (argc > 2)
-        {
-                printf("Please insert name for 1 file ONLY.\n");
-                return (1);
-        }
-        fd = open(argv[1], O_RDONLY);
-        printf("fd: %d\n", fd);
-        while ((nxt_line = get_next_line(fd)))
-                printf("%s", nxt_line);
-        return (0);
+	if (argc != 2)
+	{
+		printf("Please insert name for 1 file ONLY.\n");
+		return (1);
+	}
+	fd = open(argv[1], O_RDONLY);
+	if (fd < 0)
+	{
+		printf("Error opening file");
+		return (1);
+	}
+	printf("fd: %d\n", fd);
+	while ((nxt_line = get_next_line(fd)))
+	{
+		printf("%s", nxt_line);
+		free(nxt_line);
+	}
+	close(fd);
+	return (0);
 }
