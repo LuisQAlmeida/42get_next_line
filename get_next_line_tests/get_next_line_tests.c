9c99ac24c8c429b51bfbb804dc0bad80206ebe51
LuisQAlmeida <lmanuel-@student.42porto.com>
2025-08-05 03:31:06 +0100

    Update get_next_line_tests.c

--- get_next_line_tests/get_next_line_tests.c
+++ get_next_line_tests/get_next_line_tests.c
@@ -2,28 +2,28 @@
 #include <stdio.h>
 #include "get_next_line.h"
 
-int	main(int argc, char *argv[])
+int     main(int argc, char *argv[])
 {
-	int		fd;
-	char	*nxt_line;
+        int             fd;
+        char    *nxt_line;
 
-	if (argc != 2)
-	{
-		printf("Please insert name for 1 file ONLY.\n");
-		return (1);
-	}
-	fd = open(argv[1], O_RDONLY);
-	if (fd < 0)
-	{
-		printf("Error opening file");
-		return (1);
-	}
-	printf("fd: %d\n", fd);
-	while ((nxt_line = get_next_line(fd)))
-	{
-		printf("%s", nxt_line);
-		free(nxt_line);
-	}
-	close(fd);
-	return (0);
+        if (argc != 2)
+        {
+                printf("Please insert name for 1 file ONLY.\n");
+                return (1);
+        }
+        if (*argv[1] == '0')
+                fd = 0;
+        else if (*argv[1] == '-')
+                fd = 42;
+        else
+                fd = open(argv[1], O_RDONLY);
+        while ((nxt_line = get_next_line(fd)))
+        {
+                printf("%s", nxt_line);
+                free(nxt_line);
+        //      sleep(1);
+        }
+        close(fd);
+        return (0);
 }
